ef8b144b3ef9691b3a2951d78cba47098a5f99f2
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2021-12-04 15:04:44 +0100

    Deleting unnecessary folders

--- cmake/gdb.cmake
+++
@@ -1,6 +0,0 @@
-add_custom_target("debug"
-    COMMENT "Debugging with GDB"
-    DEPENDS ${PROJECT_NAME}
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/build
-    COMMAND gdb -tui ${PROJECT_NAME}
-)
--- cmake/test.cmake
+++
@@ -1,6 +0,0 @@
-add_custom_target("pytest"
-    COMMENT "Running PyTest tests"
-    # DEPENDS ${PROJECT_NAME}
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-    COMMAND python3 -m pytest tests/
-)
