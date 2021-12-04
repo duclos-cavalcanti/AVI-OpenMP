072bb4028de3ee3afd8363aeb08dbbc5878de45a
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2021-12-04 11:11:31 +0100

    Using Python3 to be more distro agnostic

--- cmake/test.cmake
+++ cmake/test.cmake
@@ -2,5 +2,5 @@ add_custom_target("pytest"
     COMMENT "Running PyTest tests"
     # DEPENDS ${PROJECT_NAME}
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-    COMMAND python -m pytest tests/
+    COMMAND python3 -m pytest tests/
 )
