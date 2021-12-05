95e3266c028012553a8925271af37bf93f412bcd
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2021-12-05 17:45:45 +0100

    Adjusted cpp backend python interface

--- backend/cpp_backend.py
+++ backend/cpp_backend.py
@@ -13,10 +13,22 @@ except (ModuleNotFoundError, ImportError) as e:
 # Wrappers for c++ code #
 #########################
 
-def example_func(i:int) -> (int):
-  """
-  C++ version backend
-  """
-  i = cpp_interface.lib.cffi_example(i)
+def async_value_iteration(V:np.array, PI:np.array, values:np.array, indices:np.array, indptr:np.array, shape:np.array, n_stars:int, nS:int, nA:int) -> (np.array, np.array):
+    """
+    C++ version backend
 
-  return i
+    """
+    _ffi = FFI()
+
+    V_ptr = _ffi.cast("float*", V.ctypes.data)
+    PI_ptr = _ffi.cast("int*", PI.ctypes.data)
+    values_ptr = _ffi.cast("float*", values.ctypes.data)
+    indices_ptr = _ffi.cast("int*", indices.ctypes.data)
+    indptr_ptr = _ffi.cast("int*", indptr.ctypes.data)
+
+    rows, cols = shape
+    nnz = values.__len__()
+
+    cpp_interface.lib.async_value_iteration(V_ptr, PI_ptr, values_ptr, indices_ptr, indptr_ptr, nnz, cols, rows, n_stars, nS, nA)
+
+    return V, PI
