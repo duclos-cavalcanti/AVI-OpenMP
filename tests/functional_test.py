ff926c03599ce5132784e401ded5b2bb9e3a5f06
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2021-12-06 23:14:52 +0100

    Added new tests and also compare end and start paths

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -7,6 +7,7 @@ sys.path.append('backend')
 
 import cpp_backend as backend
 import data_demo as data
+import graph
 
 
 def test_debug_data():
@@ -28,5 +29,43 @@ def test_debug_data():
 
     assert values_result.all() == values_golden.all(), "Values do not match golden debug values"
     assert policies_result.all() == policies_golden.all(), "Results do not match golden debug results"
+
+    random_state = data.state_from_tuple(max_fuel - 1, nr_stars - 1, 0, nr_stars)
+    star_graph, stars, star_types = data.load_star_values(f"data/data_small")
+    fuel, goal_star, cur_star = data.state_to_tuple(random_state, nr_stars)
+
+    a_path = graph.a_star(cur_star, goal_star, star_graph, stars)
+    pi_path = data.travel(random_state, data.to_sparse_matrix(values, indices, indptr, shape), policies_result, nr_stars, nr_actions)
+
+    assert a_path[0] == pi_path[0], "Start Values don't match"
+    assert a_path[-1] == pi_path[-1], "End Values don't match"
+
+    return
+
+def test_small_data():
+    """Description
+
+    @param param:  Description
+    @type  param:  Type
+
+    @return:  Description
+    """
+    values, indices, indptr, shape = data.load_sparse_matrix("data/data_small", "P")
+    max_fuel, nr_states, nr_actions, nr_stars = data.load_parameters("data/data_small/parameters.pickle")
+
+    value_arr = np.zeros(nr_states).astype(np.float32)
+    policy_arr = np.zeros(nr_states).astype(np.int32)
+
+    values_result, policies_result = backend.async_value_iteration(value_arr.copy(), policy_arr.copy(), values, indices, indptr, shape, nr_stars, nr_states, nr_actions)
+
+    random_state = data.state_from_tuple(max_fuel - 1, nr_stars - 1, 0, nr_stars)
+    star_graph, stars, star_types = data.load_star_values(f"data/data_small")
+    fuel, goal_star, cur_star = data.state_to_tuple(random_state, nr_stars)
+
+    a_path = graph.a_star(cur_star, goal_star, star_graph, stars)
+    pi_path = data.travel(random_state, data.to_sparse_matrix(values, indices, indptr, shape), policies_result, nr_stars, nr_actions)
+
+    assert a_path[0] == pi_path[0], "Start Values don't match"
+    assert a_path[-1] == pi_path[-1], "End Values don't match"
 
     return
