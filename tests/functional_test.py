7aa4e7195ecf3cf7e1cee16732449bae1bc48ce3
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2021-12-07 19:49:42 +0100

    Added distance check in test

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -24,7 +24,9 @@ def test_debug_data():
     value_arr = np.zeros(nr_states).astype(np.float32)
     policy_arr = np.zeros(nr_states).astype(np.int32)
 
+    # J,pi
     values_result, policies_result = backend.async_value_iteration(value_arr.copy(), policy_arr.copy(), values, indices, indptr, shape, nr_stars, nr_states, nr_actions)
+    # J_star,pi_star
     values_golden, policies_golden = data.load_results("data/data_debug")
 
     assert values_result.all() == values_golden.all(), "Values do not match golden debug values"
@@ -39,6 +41,11 @@ def test_debug_data():
 
     assert a_path[0] == pi_path[0], "Start Values don't match"
     assert a_path[-1] == pi_path[-1], "End Values don't match"
+
+    # (J - J_star).max()
+    # distance = np.abs(J - J_star).max()
+    distance = np.abs(values_result - values_golden).max()
+    assert distance < 1e-3, f"The fixed point is too far away: {distance}"
 
     return
 
@@ -56,7 +63,10 @@ def test_small_data():
     value_arr = np.zeros(nr_states).astype(np.float32)
     policy_arr = np.zeros(nr_states).astype(np.int32)
 
+    # J,pi
     values_result, policies_result = backend.async_value_iteration(value_arr.copy(), policy_arr.copy(), values, indices, indptr, shape, nr_stars, nr_states, nr_actions)
+    # J_star,pi_star
+    values_golden, policies_golden = data.load_results("data/data_small")
 
     random_state = data.state_from_tuple(max_fuel - 1, nr_stars - 1, 0, nr_stars)
     star_graph, stars, star_types = data.load_star_values(f"data/data_small")
@@ -67,5 +77,10 @@ def test_small_data():
 
     assert a_path[0] == pi_path[0], "Start Values don't match"
     assert a_path[-1] == pi_path[-1], "End Values don't match"
+
+    # (J - J_star).max()
+    # distance = np.abs(J - J_star).max()
+    distance = np.abs(values_result - values_golden).max()
+    assert distance < 1e-3, f"The fixed point is too far away: {distance}"
 
     return
