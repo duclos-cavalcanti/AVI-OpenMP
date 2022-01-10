d6353dc099073341cead9855cb02dae8d20727a6
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2022-01-10 02:39:41 +0100

    Updated to a much faster implementation

--- backend/backend/src/backend.cpp
+++ backend/backend/src/backend.cpp
@@ -2,9 +2,8 @@
 #include <iostream>
 #include <cmath>
 
-void asyncValueIteration(Eigen::Map<Eigen::VectorXf> V, Eigen::Map<Eigen::VectorXi> PI, Eigen::Map<SparseMat> probabilities, const unsigned int n_stars, const unsigned int nS, const unsigned int nA);
+void asyncValueIteration(Eigen::Ref<Eigen::VectorXf> V, Eigen::Ref<Eigen::VectorXi> PI, Eigen::Ref<SparseMat> probabilities, const unsigned int n_stars, const unsigned int nS, const unsigned int nA);
 inline void getStateCost(float& cost, int& state, int& control, const unsigned int& nr_stars);
-inline void countActions(int& actions, const int nr_actions, SparseMat middle_rows);
 inline Eigen::VectorXf getActionValues(Eigen::Ref<SparseMat> probabilities, Eigen::Ref<Eigen::Map<Eigen::VectorXf>> values, int current_state, const unsigned int& nr_actions, const unsigned int& nr_stars, const float& alpha);
 
 void asyncValueIterationWrapper(float* val_arr, int* policy_arr, float* values, 
@@ -20,22 +19,24 @@ void asyncValueIterationWrapper(float* val_arr, int* policy_arr, float* values,
   asyncValueIteration(value_map, policy_map, probabilities_map, nr_stars, nr_states, nr_actions);
 }
 
-void asyncValueIteration(Eigen::Map<Eigen::VectorXf> values, 
-                         Eigen::Map<Eigen::VectorXi> policies, 
-                         Eigen::Map<SparseMat> probabilities, 
-                         const unsigned int nr_stars, const unsigned int nr_states, 
+void asyncValueIteration(Eigen::Ref<Eigen::VectorXf> values, 
+                         Eigen::Ref<Eigen::VectorXi> policies, 
+                         Eigen::Ref<SparseMat> probabilities, 
+                         const unsigned int nr_stars, 
+                         const unsigned int nr_states, 
                          const unsigned int nr_actions) {
 
-    omp_set_num_threads(8);
+    omp_set_num_threads(4);
 
-    const double tolerance = 1e-5;
+    const double tolerance = 1e-6;
     const float alpha = 0.99;
     float delta;
     unsigned int epochs = 0;
+    unsigned int max_iter = 1e3;
 
     do {
       delta = 0;
-      #pragma omp parallel for num_threads(10)
+      #pragma omp parallel for
       for (unsigned int current_state = 0; current_state < nr_states; current_state++) {
         Eigen::VectorXf action_values = getActionValues(probabilities, values, current_state, nr_actions, nr_stars, alpha);
 
@@ -48,26 +49,31 @@ void asyncValueIteration(Eigen::Map<Eigen::VectorXf> values,
         policies[current_state] = min_index; // action needed to achieve min value
       }
       epochs += 1;
+
+      if (epochs >= max_iter)
+        break;
+
     } while (delta >= tolerance);
 }
 
 inline Eigen::VectorXf getActionValues(Eigen::Ref<SparseMat> probabilities, 
-                                Eigen::Ref<Eigen::Map<Eigen::VectorXf>> values, 
-                                int current_state, 
-                                const unsigned int& nr_actions, 
-                                const unsigned int& nr_stars, 
-                                const float& alpha) {
-  SparseMat probabilities_slice = probabilities.middleRows(current_state * nr_actions, nr_actions);
+                                       Eigen::Ref<Eigen::Map<Eigen::VectorXf>> values, 
+                                       int current_state, 
+                                       const unsigned int& nr_actions, 
+                                       const unsigned int& nr_stars, 
+                                       const float& alpha) {
+  const auto& probabilities_slice = probabilities.middleRows(current_state * nr_actions, nr_actions);
   int actions = 0;
-
-  countActions(actions, nr_actions, probabilities_slice);
+  for (unsigned int i = 0; i < nr_actions; i++)
+      if (probabilities_slice.row(i).sum() > 0)
+          actions += 1;
   
   Eigen::VectorXf action_values(actions);
   action_values.fill(0.0);
 
   #pragma omp parallel for
   for (int row = 0; row < probabilities_slice.outerSize(); ++row) {
-    for (SparseMat::InnerIterator it(probabilities_slice, row); it; ++it) {
+    for (Eigen::InnerIterator<typeof(probabilities_slice)> it(probabilities_slice, row); it; ++it) { // iterates columns
       float cost;
       float probability = it.value();
       int next_state = it.col();
@@ -102,11 +108,3 @@ inline void getStateCost(float& cost, int& state, int& control, const unsigned i
         cost = 0.0;
     }
   }
-
-inline void countActions(int& actions, const int nr_actions, SparseMat state_probs) {
-  #pragma omp parallel for num_threads(10)
-  for (unsigned int i = 0; i < nr_actions; i++) {
-      if (state_probs.row(i).sum() > 0) // val > 0, action exists
-          actions += 1;
-  }
-}
